c14d417e430139d2927c1bab9cad89994598f71e
Albert Wang <albert@cellabus.com>
2015-03-09 18:17:16 -0700

    Update Configuration to allow Procs for ssl_key, ssl_certificate, base_url

--- lib/ios-cert-enrollment/configuration.rb
+++ lib/ios-cert-enrollment/configuration.rb
@@ -18,11 +18,27 @@ module IOSCertEnrollment::Configuration
   attr_accessor *VALID_OPTIONS_KEYS
 
   def ssl_key
-    @ssl_key ||= File.read ssl_key_path
+    if @ssl_key.is_a? Proc
+      @ssl_key.call
+    else
+      @ssl_key ||= File.read ssl_key_path
+    end
   end
 
   def ssl_certificate
-    @ssl_certificate ||= File.read ssl_certificate_path
+    if @ssl_certificate.is_a? Proc
+      @ssl_certificate.call
+    else
+      @ssl_certificate ||= File.read ssl_certificate_path
+    end
+  end
+
+  def base_url
+    if @base_url.is_a? Proc
+      @base_url.call
+    else
+      @base_url
+    end
   end
 
   # Convenience method to allow configuration options to be set in a block
