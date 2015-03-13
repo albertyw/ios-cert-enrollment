8e311c17e430159cd49281242531d962ef1ece56
Albert Wang <albert@cellabus.com>
2015-03-13 13:20:07 -0700

    Delete unused test directory

--- test/test_ios-cert-enrollment.rb
+++
@@ -1,8 +0,0 @@
-require 'helper'
-
-class TestIosCertEnrollment < Test::Unit::TestCase
-  
-  # should "probably rename this file and start testing for real" do
-  #   flunk "hey buddy, you should probably rename this file and start testing for real"
-  # end
-end
